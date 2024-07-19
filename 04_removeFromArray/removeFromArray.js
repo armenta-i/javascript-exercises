2c02a409831b20c2e4734bbc66912878910b313e
armenta-i <i_armenta@outlook.com>
2024-07-18 22:33:25 -0500

    Exerices 04 completed and passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(userArray, ...numbersToRemove) {
+    let modifiedArray = userArray;
+    let item;
+    for(let i = 0; i < numbersToRemove.length; i++) {
+        item = numbersToRemove[i]
+        for(let j = userArray.length - 1; j >= 0; j--) {
+        if(userArray[j] === item){
+            modifiedArray.splice(j, 1);
+        }
+        }
+    }
+    return modifiedArray;
 };
 
 // Do not edit below this line
