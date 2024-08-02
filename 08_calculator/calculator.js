ef20a79c6d0d6f7bd608f43ce43772a9967bd54a
armenta-i <i_armenta@outlook.com>
2024-08-01 22:36:07 -0500

    Completed Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(firstInt, secondInt) {
+  return firstInt + secondInt;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstInt, secondInt) {
+  return firstInt - secondInt;
 };
 
-const sum = function() {
-	
+const sum = function(userArray) {
+  return userArray.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(userArray) {
+  return userArray.reduce((acc, num) => acc * num, 1);
 };
 
-const power = function() {
-	
+const power = function(intToExpt, exponential) {
+  return intToExpt ** exponential;
 };
 
-const factorial = function() {
-	
+const factorial = function(intToFactorial) {
+    let result = 1;
+    while(intToFactorial > 0){
+      result *= intToFactorial;
+      intToFactorial--;
+    }
+    return result
 };
 
 // Do not edit below this line
